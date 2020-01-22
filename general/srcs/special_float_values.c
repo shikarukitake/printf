8353598f0e40c395b521bb9ca1a3938f148f8aa5
ayellin <ayellin@student.21-school.ru>
2020-01-22 19:44:32 +0300

    norm 2

--- general/srcs/special_float_values.c
+++ general/srcs/special_float_values.c
@@ -1,55 +1,56 @@
-//
-// Created by Aletha Yellin on 21/01/2020.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   special_float_values.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayellin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/22 19:35:23 by ayellin           #+#    #+#             */
+/*   Updated: 2020/01/22 19:43:22 by ayellin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "special_float_values.h"
 
-int is_inf(t_ldc ldc)
+int			is_inf(t_ldc ldc)
 {
 	return (ldc.parts.e == 32767u &&
 			ldc.parts.m == 9223372036854775808ul &&
 			ldc.parts.s == 0u);
 }
 
-int is_ninf(t_ldc ldc)
+int			is_ninf(t_ldc ldc)
 {
 	return (ldc.parts.e == 32767u &&
 			ldc.parts.m == 9223372036854775808ul &&
 			ldc.parts.s == 1u);
 }
 
-int is_nan(t_ldc ldc)
+int			is_nan(t_ldc ldc)
 {
 	return (ldc.parts.e == 32767u &&
 			(ldc.parts.m == 13835058057429647360ul || ldc.parts.m == 8u));
 }
 
-int is_reserved_value(t_ldc ldc, long double ld, char *fbuf)
+static int	copy(char *fbuf, char *value)
+{
+	ft_strcpy(fbuf, value);
+	return (1);
+}
+
+int			is_reserved_value(t_ldc ldc, long double ld, char *fbuf)
 {
 	if (ld == 0 && ldc.parts.s == 1u)
-	{
-		ft_strcpy(fbuf, "-0.0");
-		return (1);
-	} else if (ld == 0 || ld == LDBL_MIN)
-	{
-		ft_strcpy(fbuf, "0.0");
-		return (1);
-	} else if (ld == - LDBL_MIN)
-	{
-		ft_strcpy(fbuf, "-0.0");
-		return (1);
-	} else if (is_inf(ldc))
-	{
-		ft_strcpy(fbuf, "inf");
-		return (1);
-	} else if (is_ninf(ldc))
-	{
-		ft_strcpy(fbuf, "-inf");
-		return (1);
-	} else if (is_nan(ldc))
-	{
-		ft_strcpy(fbuf, "nan");
-		return (1);
-	}
+		return (copy(fbuf, "-0.0"));
+	else if (ld == 0 || ld == LDBL_MIN)
+		return (copy(fbuf, "0.0"));
+	else if (ld == -LDBL_MIN)
+		return (copy(fbuf, "-0.0"));
+	else if (is_inf(ldc))
+		return (copy(fbuf, "inf"));
+	else if (is_ninf(ldc))
+		return (copy(fbuf, "-inf"));
+	else if (is_nan(ldc))
+		return (copy(fbuf, "nan"));
 	return (0);
 }
